36909b916ba4c548236b798cacd5a90fd87799e8
sivabheri <sivabheri8@gmail.com>
2025-01-06 19:02:37 +0530

    LCS variations Part-1

--- Dp/LC_substring.py
+++ Dp/LC_substring.py
@@ -1,7 +1,11 @@
 # Longest Common Substring - LCS variation
 
-#User function Template for python3
-
+''' 
+We have to print the Longest common Substing in s1 and s2.
+It can be done by tracking the ending index of the longest common substring.
+Then the substring = any_string[starting_index->end_index]
+starting_index =  end_index - Max_len(longest Common Substring) 
+'''
 class Solution:
     def length(self, s1, s2):
         # code here
@@ -33,6 +37,36 @@ class Solution:
         
         return max_len
 
+    def printLCstring(self,s1,s2):
+
+        n = len(s1)
+        m = len(s2)
+        
+        # tabulation
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        
+        # base condition
+        for i in range(n+1):
+            for j in range(m+1):
+                if i==0 or j==0: # when either of the strings is empty then no LCstring
+                    dp[i][j]=0
+        # store the max length
+        max_len = 0
+        end_index = 0 # ending index of the logest common substring
+        # choice diagram
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                    if dp[i][j]>max_len:
+                        max_len = dp[i][j]
+                        end_index = i
+                else:
+                    dp[i][j] = 0 # Discontinuity        
+        startInd = end_index - max_len
+        string = s1[startInd:end_index]
+        return string 
+
 
 #{ 
  # Driver Code Starts
@@ -45,5 +79,6 @@ if __name__ == '__main__':
         S2 = input().strip()
         ob = Solution()
         print(f'The Length of Longest common Substring is : {ob.length(S1, S2)}')
+        print(f'The Longest common Substring is: {ob.printLCstring(S1,S2)}')
 
 # } Driver Code Ends
